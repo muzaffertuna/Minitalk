dfea50bf11e32f80c9b9a24ed17e385504db65d7
muzaffertuna <toktastuna@gmail.com>
2023-09-10 18:30:43 +0300

    merge

---
+++ client.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtoktas <mtoktas@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 19:11:53 by mtoktas           #+#    #+#             */
+/*   Updated: 2023/09/10 18:12:40 by mtoktas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+int ft_atoi(char *str)
+{
+    int res;
+    int sign;
+    int i;
+
+    res = 0;
+    sign = 1;
+    i = 0;
+    if (str[0] == '-'){
+        sign *= -1;
+        i++;
+    }
+    while (str[i])
+    {
+        res = res * 10 + str[i] - 48;
+        i++;
+    }
+    return(res * sign);
+}
+
+int main(int ac, char **av)
+{
+    int i;
+    int j;
+    int server_id;
+    if(ac == 3)
+    {
+        server_id = ft_atoi(av[1]);
+        i = 0;
+        while(av[2][i])
+        {
+            j = 0;
+            while(j != 8)
+            {
+                if(av[2][i] >> j & 1)
+                    kill(server_id, SIGUSR1);
+                else
+                    kill(server_id, SIGUSR2);
+                    usleep(100);
+                j++;
+            }
+            i++;
+        }
+    }else{
+        printf("NOT CORRECT ARGUMENTS\n");
+        return (0);
+    }
+}
---
+++ server.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtoktas <mtoktas@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 19:08:37 by mtoktas           #+#    #+#             */
+/*   Updated: 2023/09/10 18:19:11 by mtoktas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void handlesignal(int sign)
+{
+    static int bit;
+    static int set;
+    
+    if (sign == SIGUSR1)
+        set += 1 << bit;
+    if (bit == 7)
+    {
+        write(1, &set, 1);
+        bit = 0;
+        set = 0;
+    }else{
+        bit++;
+    }
+}
+
+void putnbr(int n)
+{
+    char c;
+
+    if(n < 10)
+    {
+        c = n + 48;
+        write(1, &c, 1);
+    }
+    else if (n > 9)
+    {
+        putnbr(n / 10);
+        putnbr(n % 10);    
+    }
+}
+
+int main(int ac, char **av)
+{
+    (void) av;
+    int p_id;
+
+    if (ac == 1)
+    {
+    p_id = getpid();
+    putnbr(p_id);
+    write(1, "\n", 1);
+    signal(SIGUSR1, handlesignal);
+    signal(SIGUSR2, handlesignal);
+    
+    while(1)
+        pause();
+    }
+    else
+        printf("No need argument.\n");
+}
